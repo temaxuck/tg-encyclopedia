3d794138b6623bd75270d7c0fb3fff5299354f4b
temaxuck <gorropand@gmail.com>
2023-10-19 13:07:51 +0700

    Functions now return Message

--- app/services/telegram_service.py
+++ app/services/telegram_service.py
@@ -2,6 +2,7 @@ import asyncio
 from app.services.bots.telegram_bot import TelegramBot
 from io import BytesIO
 from telegram.constants import ParseMode
+from telegram import Message
 
 
 class TelegramService:
@@ -11,37 +12,45 @@ class TelegramService:
 
     @staticmethod
     def post_message_to_channel(
-        telegram_bot: TelegramBot, channel_id: str, text: str
-    ) -> None:
+        telegram_bot: TelegramBot,
+        channel_id: str,
+        text: str,
+        reply_to_message_id: int = None,
+    ) -> Message:
         """Post message to the telegram channel
 
         Args:
             telegram_bot (TelegramBot): telegram bot entity
             channel_id (str): telegram channel id
             text (str): text message to post
+            reply_to_message_id <Optional> (int): message id to reply
         """
 
-        asyncio.run(telegram_bot.bot.send_message(chat_id=channel_id, text=text))
+        return asyncio.run(
+            telegram_bot.bot.send_message(
+                chat_id=channel_id, text=text, reply_to_message_id=reply_to_message_id
+            )
+        )
 
     @staticmethod
     def post_image_to_channel(
         telegram_bot: TelegramBot,
         channel_id: str,
         image: BytesIO,
-        caption: str,
+        caption: str = None,
         parse_mode: ParseMode = ParseMode.HTML,
-    ) -> None:
+    ) -> Message:
         """Post message to the telegram channel
 
         Args:
             telegram_bot (TelegramBot): telegram bot entity
             channel_id (str): telegram channel id
             image (BytesIO): image represented in BytesIO object
-            caption (str): text going along with provided image
+            caption <Optional> (str): text going along with provided image
             parse_mode <Optional> (ParseMode): caption's parse_mode, by default ParseMode.HTML
         """
 
-        asyncio.run(
+        return asyncio.run(
             telegram_bot.bot.send_photo(
                 chat_id=channel_id, photo=image, caption=caption, parse_mode=parse_mode
             )
