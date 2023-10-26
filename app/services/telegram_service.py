c04637323060194505fec6023c2e4ad685d8fc87
temaxuck <gorropand@gmail.com>
2023-10-26 11:38:42 +0700

    Allow passing any arguments to TelegramService methods

--- app/services/telegram_service.py
+++ app/services/telegram_service.py
@@ -1,7 +1,6 @@
 from app.services.bots.telegram_bot import TelegramBot
 from io import BytesIO
-from telegram.constants import ParseMode
-from telegram import Message
+from telegram import Message, InputMediaPhoto
 
 
 class TelegramService:
@@ -11,47 +10,62 @@ class TelegramService:
 
     @staticmethod
     async def post_message_to_channel(
-        telegram_bot: TelegramBot,
-        channel_id: str,
-        text: str,
-        reply_to_message_id: int = None,
-        parse_mode: ParseMode = ParseMode.HTML,
+        telegram_bot: TelegramBot, channel_id: str, text: str, *args, **kwargs
     ) -> Message:
         """Post message to the telegram channel
 
         Args:
             telegram_bot (TelegramBot): telegram bot entity
-            channel_id (str): telegram channel id
+            channel_id (str): telegram channel id to post to
             text (str): text message to post
-            reply_to_message_id <Optional> (int): message id to reply
-            parse_mode <Optional> (ParseMode): text's parse_mode, by default ParseMode.HTML
+
+        Returns:
+            message (Message): telegram object Message which represents posted message to telegram chat
         """
 
         return await telegram_bot.bot.send_message(
-            chat_id=channel_id,
-            text=text,
-            reply_to_message_id=reply_to_message_id,
-            parse_mode=parse_mode,
+            chat_id=channel_id, text=text, *args, **kwargs
         )
 
     @staticmethod
     async def post_image_to_channel(
-        telegram_bot: TelegramBot,
-        channel_id: str,
-        image: BytesIO,
-        caption: str = None,
-        parse_mode: ParseMode = ParseMode.HTML,
+        telegram_bot: TelegramBot, channel_id: str, image: BytesIO, *args, **kwargs
     ) -> Message:
         """Post message to the telegram channel
 
         Args:
             telegram_bot (TelegramBot): telegram bot entity
-            channel_id (str): telegram channel id
+            channel_id (str): telegram channel id to post to
             image (BytesIO): image represented in BytesIO object
-            caption <Optional> (str): text going along with provided image
-            parse_mode <Optional> (ParseMode): caption's parse_mode, by default ParseMode.HTML
+
+        Returns:
+            message (Message): telegram object Message which represents posted message to telegram chat
         """
 
         return await telegram_bot.bot.send_photo(
-            chat_id=channel_id, photo=image, caption=caption, parse_mode=parse_mode
+            chat_id=channel_id, photo=image, *args, **kwargs
+        )
+
+    @staticmethod
+    async def post_images_to_channel(
+        telegram_bot: TelegramBot,
+        channel_id: str,
+        media_group: list[InputMediaPhoto],
+        *args,
+        **kwargs
+    ) -> tuple[Message]:
+        """
+        Post image to telegram channel
+
+        Args:
+            telegram_bot (TelegramBot): telegram bot entity
+            channel_id (str): telegram channel id to post to
+            images (list[InputMediaPhoto]): list of InputMediaPhoto objects
+
+        Returns:
+            messages (tuple[Message]): tuple of telegram objects Message which represent posted messages to telegram chat
+        """
+
+        return await telegram_bot.bot.send_media_group(
+            chat_id=channel_id, media=media_group, *args, **kwargs
         )
