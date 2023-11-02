1409808a38fbc245cbe4cf16c746d36e0990da31
temaxuck <gorropand@gmail.com>
2023-11-02 12:59:24 +0700

    [Temporary] Loading batches of pyramid

--- run.py
+++ run.py
@@ -12,9 +12,17 @@ def run_bot():
         channel_id=Config.TELEGRAM_CHANNEL_ID,
         oenp_api_url=Config.OENP_API_URL,
         oeis_api_url="https://oeis.org/",
+        telegram_post_format=Config.TELEGRAM_POST_FORMAT,
     )
 
-    asyncio.run(bot.post_pyramids_to_channel(range(1, 2000), latency=60))
+    async def load_batches():
+        await bot.post_pyramids_to_channel(range(1, 10), latency=60)
+        await bot.post_pyramid_to_channel(484)
+        await bot.post_pyramid_to_channel(range(600, 620), latency=60)
+        await bot.post_pyramid_to_channel(range(1000, 1020), latency=60)
+        await bot.post_pyramid_to_channel(range(1450, 1500), latency=60)
+
+    asyncio.run(load_batches())
 
 
 run_bot()
